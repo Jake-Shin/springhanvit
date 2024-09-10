9b52c18c1378dff32e9be5e69d578e8cf6b49752
Jake-Shin <44884955+Jake-Shin@users.noreply.github.com>
2024-09-10 16:46:24 +0900

    자료형 double로 수정

--- src/main/resources/static/createTable.sql
+++ src/main/resources/static/createTable.sql
@@ -6,7 +6,7 @@ CREATE DATABASE `hanvithancom`
 CREATE TABLE `hancom` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
-  `completionTime` varchar(100) DEFAULT NULL,
+  `completionTime` double DEFAULT NULL,
   `create_datetime` datetime DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
